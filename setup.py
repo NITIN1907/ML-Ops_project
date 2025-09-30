5bed9c408e014662220da7d06279d2b486100485
NITIN1907 <nitindogra1907@gmail.com>
2025-10-01 02:23:02 +0530

    project structure ready

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "src",
+    version="0.0.1",
+    author="nitin dogra",
+    author_email="nitindogra1907@gmail.com",
+    packages=find_packages()
+
+)
